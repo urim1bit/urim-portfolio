da0f9c34c3a677108291e9ae6e0d98d86801b1ff
urim1bit <mustafa.urim1@gmail.com>
2026-04-04 16:20:19 +0200

    syyy

--- server.js
+++ server.js
@@ -4,10 +4,39 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect any .html requests to clean URLs
+app.use((req, res, next) => {
+  if (req.path.endsWith('.html')) {
+    const cleanPath = req.path.slice(0, -5); // remove .html
+    return res.redirect(301, cleanPath || '/');
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/paytrack', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'paytrack.html'));
+});
+
+app.get('/privacy', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'privacy.html'));
+});
+
+app.get('/story', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'story.html'));
+});
+
+app.get('/account-deletion', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'account-deletion.html'));
+});
+
+app.get('/materialyou', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'materialyou.html'));
 });
 
 app.listen(PORT, () => {
